42c1d83457b8334641907890fbb1c6e0217fb448
NFT <nft@yuriui-iMac.local>
2022-04-01 16:13:20 +0900

    initial

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const TodoList = artifacts.require("./TodoList.sol");
+//const TodoList = artifacts.require("TodoList"); // Enter your Contract Name not File name
+
+module.exports = function(deployer) {
+  deployer.deploy(TodoList);
+};
